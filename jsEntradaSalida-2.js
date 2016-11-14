578340642162c4503cc241981c36dcfcaa11a742
fernandeztrevor <fernandeztrevor@outlook.com>
2016-11-14 11:44:09 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+var apellido="Fernández";
+var nombre;
+nombre=prompt("Ingrese su nombre","nn");
+alert("Mi nombre es: "+nombre+" y su apellido es "+apellido);	
+}
+
