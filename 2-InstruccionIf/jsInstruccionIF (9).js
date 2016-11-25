f684fad0be3ecb42e08ceae164df576d58f9f6d9
fernandeztrevor <fernandeztrevor@outlook.com>
2016-11-25 11:42:06 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,8 +1,18 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	var random
-	random=Math.floor((Math.random()*10+1))
-	alert (random)
+	var random;
+	random=Math.floor((Math.random()*10+1));
+	alert (random);
+
+/* entre 20 y 30
+var random;
+	random=Math.floor((Math.random()*10)+20);
+	alert (random);
+entre 5 y 8 inclusive
+	var random;
+	random=Math.floor((Math.random()*4)+5);
+	alert (random);
+	*/
 
 }//FIN DE LA FUNCIÓN
