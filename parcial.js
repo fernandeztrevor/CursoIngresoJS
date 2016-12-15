553b4a05ae3467c2afa53971ae99b0987ec685ad
fernandeztrevor <fernandeztrevor@outlook.com>
2016-12-15 17:10:40 -0300

    Add files via upload

---
+++ parcial.js
@@ -0,0 +1,53 @@
+//nivel 1
+//ejercicio 1
+
+var lado=document.getElementbyID("Lado").value;
+var perimetro=parseint(lado)*4;
+alert("El prerimetro del cuadrado es: "+perimetro);
+
+//ejercicio 2
+var importe=prompt(Ingrese el importe);
+importeFinal=parseInt(importe)*1.21;
+alert("El importe final es: "+importeFinal);
+
+//ejercicio 3
+var largo=document.getElementbyId('Largo').value;
+var ancho=document.getElementbyId('Ancho').value;
+perimetro=[parseInt(largo)*6]+[parseInt(ancho)*6];
+alert("Usted necesita "+perimetro+"m de alambrado");
+
+//nivel 2
+//ejercicio 4
+var numeroUno=prompt(Ingrese el primer numero);
+var numeroDos=prompt(Ingrese el segundo numero);
+var resultado
+if(numeroUno==numeroDos)
+{
+	resultado=numeroUno*numero2
+}
+if(numeroUno>numeroDos)
+{
+	resultado=numeroUno-numeroDos
+}
+else
+{
+	resultado=parseInt(numeroUno)+parseInt(numeroDos)
+};
+document.write('resultado');
+
+//ejercicio 5
+var dia=prompt(ingrese el dia);
+switch(dia)
+{
+	case "sabado":
+	case "domingo":
+	alert("Es fin de semana")
+	break;
+	default
+	alert("A trabajar!");
+}
+
+//nivel3
+
+//ejercicio 6
+
